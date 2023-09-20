666e998c5c6f27df9175e800ace6288f85028423
Willypai <willypai0329@gmail.com>
2023-09-20 14:46:43 +0800

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,38 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-interface switch_type{
-    double[][] convert(int[][] intArrary);
+interface Mapping {
+    double[][] transform (int[][] intArray);
 }
-class switch2square implements switch_type{
+
+
+
+class Map implements Mapping {
     @Override
-    public  double[][] convert(int [][] intArray){
-        int rows = intArray.length;
-        int maxCols = 0 ;
-        for (int i = 0; i < rows; i++) {
-            int currentCols = intArray[i].length;
-            if(currentCols > maxCols){
-                maxCols =currentCols;
+    public double[][] transform(int[][] intArray) {
+        double[][] doubleArray = new double[intArray.length][];
+        for (int i = 0; i < intArray.length; i++) {
+            doubleArray[i] = new double[intArray[i].length];
+            for (int j = 0; j < intArray[i].length; j++) {
+                doubleArray[i][j] = intArray[i][j] * 0.5;
             }
-
         }
-        double[][] doubleArray = new double[rows][maxCols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < maxCols; j++) {
-                doubleArray [i][j] = (double)intArray[i][j]*0.5;
-
-            }
-
-        }
-
         return doubleArray;
     }
 }
+
 public class Main {
     public static void main(String[] args) {
-        int[][] intArray = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9},
-                {3,2,1},{6,5,4},{9,8,7},
-                {1,1,1},{2,2,2},{3,3,3}
-        };
-        switch_type switchType = new switch2square();
-        double[][] doubleArray = switchType.convert(intArray);
+        int[][] intArray = {{1, 2, 3}, {4, 5},
+                {3, 2, 1}, {6, 5, 4}, {9, 8, 7},
+                {1, 1, 1}, {2, 2}, {3, 3, 3}};
+
+        Mapping map = new Map();
+
+        double[][] doubleArray = map.transform(intArray);
         for (int i = 0; i < doubleArray.length; i++) {
             System.out.print("{  ");
             for (int j = 0; j < doubleArray[i].length; j++) {
-                System.out.print(doubleArray[i][j] + "  ");
+                System.out.print(doubleArray[i][j] + " ");
             }
-
-            System.out.println("}");
+            System.out.println(" }");
         }
     }
 }
